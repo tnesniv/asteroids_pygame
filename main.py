ea5feb225684ccc928e845ae35ff4a29837cb396
Circle_of_Ossus <zoobastad@gmail.com>
2025-06-14 16:44:05 +0200

    Black screen added. Can terminate it too

--- main.py
+++ main.py
@@ -2,10 +2,17 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
-
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
