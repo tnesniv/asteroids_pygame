1818fb5ffa3d9abb6c81312018dd579cbae1b144
Circle_of_Ossus <zoobastad@gmail.com>
2025-06-15 18:12:40 +0200

    added circleshape.py

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    timer = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        # limit fps to 60
+        dt = timer.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
