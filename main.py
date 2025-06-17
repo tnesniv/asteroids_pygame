7e5ded11280245691d3c0c240f04acaf59027331
Circle_of_Ossus <zoobastad@gmail.com>
2025-06-17 11:34:42 +0200

    Added asteroids and asteroid fields. Made container groups to draw and update all objects

--- main.py
+++ main.py
@@ -4,20 +4,32 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     timer = pygame.time.Clock()
     dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    AsteroidField.containers = (updateable,)
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        player.update(dt)
-        player.draw(screen)
+        updateable.update(dt)
+        for i in drawable:
+            i.draw(screen)
 
         # reset screen?
         pygame.display.flip()
